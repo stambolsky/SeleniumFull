2fa04e0c5081647d6f03d65440433c2516bc6e89
Borman666 <Borman010884@yandex.ru>
2020-04-25 19:36:26 +0300

    3. Запуск разных браузеров

--- src/test/java/com/stqa/MyFirstTest.java
+++ src/test/java/com/stqa/MyFirstTest.java
@@ -2,14 +2,16 @@ package com.stqa;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
 public class MyFirstTest extends TestBase{
 
     @Test
-    public void myFirstTest() {
+    public void myFirstTest() throws InterruptedException {
         driver.get("http://www.google.com/");
         driver.findElement(By.name("q")).sendKeys("webdriver");
+        Thread.sleep(100);
         driver.findElement(By.name("btnK")).click();
         wait.until(titleIs("webdriver - Поиск в Google"));
     }
--- src/test/java/com/stqa/TestBase.java
+++ src/test/java/com/stqa/TestBase.java
@@ -1,27 +1,49 @@
 package com.stqa;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
+    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
     public WebDriver driver;
     public WebDriverWait wait;
+    public String browser = "CHROME";
 
     @Before
     public void start() {
-        driver = new ChromeDriver();
+        if (tlDriver.get() != null) {
+            driver = tlDriver.get();
+            wait = new WebDriverWait(driver, 10);
+            return;
+        }
+        switch (browser) {
+            case "CHROME":
+                WebDriverManager.chromedriver().setup();
+                driver = new ChromeDriver();
+                break;
+            case "FIREFOX":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new FirefoxDriver();
+                break;
+            case "SAFARI":
+                driver = new SafariDriver();
+                break;
+        }
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
         wait = new WebDriverWait(driver, 10);
     }
 
-    @After
+    @After()
     public void stop() {
         driver.quit();
         driver = null;
