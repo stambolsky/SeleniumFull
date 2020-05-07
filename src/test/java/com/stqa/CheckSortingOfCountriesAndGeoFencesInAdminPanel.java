0842e5bc1dac661c525abeaae02d680b79c9bfc5
Borman666 <Borman010884@yandex.ru>
2020-05-07 13:59:30 +0300

    FIXED 2 Задание 9. Проверить сортировку стран и геозон в админке

--- src/test/java/com/stqa/CheckSortingOfCountriesAndGeoFencesInAdminPanel.java
+++ src/test/java/com/stqa/CheckSortingOfCountriesAndGeoFencesInAdminPanel.java
@@ -3,6 +3,8 @@ package com.stqa;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -61,48 +63,34 @@ public class CheckSortingOfCountriesAndGeoFencesInAdminPanel extends TestBase {
     }
 
     @Test
-    public void checkSortGeoZones() {
+    public void checkSortGeoZones() throws InterruptedException {
         driver.get("http://localhost:8888/litecart/admin/?app=geo_zones&doc=geo_zones");
         driver.findElement(By.name("username")).sendKeys("admin");
         driver.findElement(By.name("password")).sendKeys("admin");
         driver.findElement(By.name("login")).click();
-        List<WebElement> geoZone = driver.findElements(By.cssSelector("form .dataTable td:nth-child(3)>a"));
-        List<String> geoZoneSortList = new ArrayList<String>();
-        List<String> geoZoneUnSortList = new ArrayList<String>();
-        for (WebElement name : geoZone) {
-            geoZoneUnSortList.add(name.getText());
-            geoZoneSortList.add(name.getText());
+        List<WebElement> countries = driver.findElements(By.cssSelector("form .dataTable td:nth-child(3)>a"));
+        List<String> countriesList = new ArrayList<String>();
+        for (WebElement name : countries) {
+            countriesList.add("//a[.='"+name.getText()+"']");
         }
 
-        List<WebElement> zones = driver.findElements(By.xpath("//form[@name='geo_zones_form']//tr[@class='odd' or @class='even']/td[4]"));
-        List<String> locator = new ArrayList<String>();
-        int number = 0;
-        for (WebElement zone : zones) {
-            if (Integer.parseInt(zone.getText()) > 0) {
-                locator.add("//form//tr[" + number + "]//a");
-            }
-            number++;
-        }
+        for (String name : countriesList) {
 
-        for (String loc : locator) {
-            driver.findElement(By.xpath(loc)).click();
-            List<WebElement> zonesList = driver.findElements(By.xpath("//table[@class='dataTable']//td[2]"));
+            Thread.sleep(500);
+            driver.findElement(By.xpath(name)).click();
+            List<WebElement> zonesList = driver.findElements(By.xpath("//form//td[3]//option[@selected='selected']"));
             List<String> zonesSortList = new ArrayList<String>();
             List<String> zonesUnSortList = new ArrayList<String>();
-            for (WebElement name : zonesList) {
-                zonesUnSortList.add(name.getText());
-                zonesSortList.add(name.getText());
+            for (WebElement zone : zonesList) {
+                zonesUnSortList.add(zone.getText());
+                zonesSortList.add(zone.getText());
             }
             Collections.sort(zonesSortList);
             for (int i=0; i<zonesList.size();i++) {
                 assertEquals("Неправильная сортировка зон", zonesUnSortList.get(i), zonesSortList.get(i));
             }
+            Thread.sleep(500);
             driver.findElement(By.xpath("//button[@name='cancel']")).click();
-        }
-
-        Collections.sort(geoZoneSortList);
-        for (int i=0; i<geoZone.size();i++) {
-            assertEquals("Неправильная сортировка зон", geoZoneUnSortList.get(i), geoZoneSortList.get(i));
         }
     }
 }
