6ba582fb90ac7e40cd369fcd61e1cfdbbdfad533
Borman666 <Borman010884@yandex.ru>
2020-05-19 18:53:40 +0300

    Добавил протоколирование действий

--- src/test/java/com/stqa/TestBase.java
+++ src/test/java/com/stqa/TestBase.java
@@ -3,20 +3,41 @@ package com.stqa;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.events.AbstractWebDriverEventListener;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
 public class TestBase {
 
-    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
-    public WebDriver driver;
+    public static ThreadLocal<EventFiringWebDriver> tlDriver = new ThreadLocal<>();
+    public EventFiringWebDriver driver;
     public WebDriverWait wait;
     public String browser = "CHROME";
+
+    public static class MyListener extends AbstractWebDriverEventListener {
+        @Override
+        public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by);
+        }
+
+        @Override
+        public void afterFindBy(By by, WebElement element, WebDriver driver) {
+            System.out.println(by +  " found");
+        }
+
+        @Override
+        public void onException(Throwable throwable, WebDriver driver) {
+            System.out.println(throwable);
+        }
+    }
 
     @Before
     public void start() {
@@ -28,16 +49,17 @@ public class TestBase {
         switch (browser) {
             case "CHROME":
                 WebDriverManager.chromedriver().setup();
-                driver = new ChromeDriver();
+                driver = new EventFiringWebDriver(new ChromeDriver());
                 break;
             case "FIREFOX":
                 WebDriverManager.firefoxdriver().setup();
-                driver = new FirefoxDriver();
+                driver = new EventFiringWebDriver(new FirefoxDriver());
                 break;
             case "SAFARI":
-                driver = new SafariDriver();
+                driver = new EventFiringWebDriver(new SafariDriver());
                 break;
         }
+        driver.register(new MyListener());
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
